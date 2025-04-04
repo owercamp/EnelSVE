a9cc5c177b45bce7630cff2efb07b65ad3db34d4
Ower A Campos <owerion22@gmail.com>
2025-04-04 11:10:29 -0500

    feat(services): se agregan caracteristicas
    se crea la función counterCentral, normalizeText y unique

--- frontend/src/services/services.ts
+++ frontend/src/services/services.ts
@@ -1,3 +1,21 @@
+const unique = (information: any, position: number) => {
+  const data: string[] = [];
+  const cleanedDataSet = new Set<string>();
+  const exp = /[^a-zA-Z]/g;
+
+  information.forEach((items: any) => {
+    const originalData = items[position];
+    const cleanedData = originalData.replace(exp, '').toUpperCase().trim();
+
+    if (!cleanedDataSet.has(cleanedData)) {
+      data.push(originalData.toUpperCase().trim());
+      cleanedDataSet.add(cleanedData);
+    }
+  });
+
+  return data;
+};
+
 const filtered = (state: string, registers: any, position: number) => {
   const filtrado = registers.filter((subArray: any) => {
     const element = subArray[position];
@@ -12,11 +30,25 @@ const filteredDifferent = (state: string[], registers: any[], position: number) 
     const element = subArray[position];
 
     const containsState = element && state.some(stateValue =>
-      String(element).toLowerCase().includes(stateValue.toLowerCase())
+      String(element).toUpperCase().includes(stateValue.toUpperCase())
     );
 
     return element && !containsState;
   });
 };
 
-export { filtered, filteredDifferent };
+const counterCentral = (centrals: string[], registers: any) => {
+  let result: any = [];
+  centrals.forEach((element: string) => {
+    result.push(registers.filter((register: string) => String(register[6]).toUpperCase().trim() === String(element).toUpperCase().trim()).length);
+  });
+  return result;
+}
+
+const normalizeText = (text: string) => {
+  return text.normalize("NFD")
+    .replace(/([aeiou])([\u0300-\u036f])/gi, '$1')
+    .normalize("NFC");
+}
+
+export { filtered, filteredDifferent, unique, counterCentral, normalizeText };
