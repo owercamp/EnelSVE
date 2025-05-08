194ec9c8b82b12a1a0cb7098cb7a8649463a41cc
Ower A Campos <owerion22@gmail.com>
2025-05-08 15:40:39 -0500

    feat(GraphForCentral): grafica por centrales
    se crea el componente de las graficas por centrales

---
+++ frontend/src/components/GraphForCentral.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+import { IGraphInform } from '../../interfaces/interfaces'
+
+const GraphForCentral: FC<IGraphInform> = ({ }) => {
+  return (
+    <div>GraphForCentral</div>
+  )
+}
+
+export default GraphForCentral
