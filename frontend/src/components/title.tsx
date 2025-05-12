a92a239d109741698b71facc7854418828a0e9aa
Ower A Campos <owerion22@gmail.com>
2025-05-12 12:50:26 -0500

    chore(title): implementación interfaz

--- frontend/src/components/title.tsx
+++ frontend/src/components/title.tsx
@@ -1,9 +1,9 @@
 import { Component, ReactNode } from "react";
 import Container from "./container";
 import React from "react";
-import { IGeneral } from "../../interfaces/interfaces";
+import ITitle from "../../interfaces/ITitle";
 
-class TitleApp extends Component<IGeneral> {
+class TitleApp extends Component<ITitle> {
   render(): ReactNode {
     const variant_text = `text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl`;
     const styles = `inline-block w-full sm:w-auto flex justify-center items-center bg-soa-light-700 dark:bg-soa-dark-900 dark:text-white text-gray-200 px-4 sm:px-6 md:px-8 ${variant_text}`;
